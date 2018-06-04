b21cddf15554a742c796b405ce6801be3768e156
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-06-04 16:32:23 +0200

    bounce gravity added

--- html/bounce/script.js
+++ html/bounce/script.js
@@ -5,18 +5,38 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 let pos = new Vector2d(100,100)
-let vel = new Vector2d(2,2);
+let vel = new Vector2d(3,0);
+let acc = new Vector2d(0,1);
 
-let ball = new Point(100,100,10,"red");
+let ball = new Point(100,100,50,"red");
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
-
+  vel.add(acc);
   pos.add(vel);
   ball.position = pos;
   ball.draw(context);
-  console.log(ball.position);
+
+  if(pos.dx < ball.r){
+    vel.dx = - vel.dx;
+    pos.dx = ball.r;
+  }
+
+  if(pos.dx > canvas.width - ball.r){
+    vel.dx = - vel.dx;
+    pos.dx =canvas.width - ball.r
+  }
+
+  if(pos.dy < ball.r){
+    vel.dy = - vel.dy;
+    pos.dy = ball.r;
+  }
+
+  if(pos.dy > canvas.height - ball.r){
+    vel.dy = - vel.dy;
+    pos.dy = canvas.height - ball.r;
+  }
 }
 
 animate();
