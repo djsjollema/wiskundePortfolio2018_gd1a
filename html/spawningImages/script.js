d3d12d2deb93603b937bf0e0c54951494ce341bf
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-06-25 16:16:50 +0200

    spawningImages speedbird

--- html/spawningImages/script.js
+++ html/spawningImages/script.js
@@ -14,8 +14,8 @@ canvas.height = window.innerHeight;
 image.addEventListener('load',()=>{
   bird = {};
   bird.image = image;
-  bird.pos = new Vector2d(500,300);
-  bird.vel = new Vector2d(3,0);
+  bird.pos = new Vector2d(canvas.width,300);
+  bird.vel = new Vector2d(-10,0);
 
   animate();
 });
@@ -24,5 +24,9 @@ function animate(){
   context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(animate);
   bird.pos.add(bird.vel);
+  if(bird.pos.dx < -100){
+    bird.pos.dx = canvas.width;
+    bird.pos.dy = Math.random()*canvas.height;
+  }
   context.drawImage(bird.image,bird.pos.dx,bird.pos.dy,100,100);
 }
