bf05c18755b4da00d416443d63cd0f8fc6674030
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-06-26 11:01:53 +0200

    spawningImages sound added

--- html/spawningImages/script.js
+++ html/spawningImages/script.js
@@ -4,8 +4,12 @@ const context = canvas.getContext('2d');
 let bird;
 
 let image = new Image();
-image.src = "bird.png";
+image.src = "myLittlePony.png";
 image.height = 100;
+
+//let shot = new Audio("shot.wav");
+let shot = new Audio('shot.wav');
+
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -14,19 +18,30 @@ canvas.height = window.innerHeight;
 image.addEventListener('load',()=>{
   bird = {};
   bird.image = image;
-  bird.pos = new Vector2d(canvas.width,300);
-  bird.vel = new Vector2d(-10,0);
+  bird.pos = new Vector2d(-100,300);
+  bird.vel = new Vector2d(10,0);
 
   animate();
 });
+
+window.addEventListener('mousedown',(evt)=>{
+  shot.play();
+  let mouseX = evt.clientX;
+  let mouseY = evt.clientY;
+  if(mouseX > bird.pos.dx && mouseX < bird.pos.dx + 200 && mouseY > bird.pos.dy && mouseY < bird.pos.dy + 200){
+    bird.pos.dx = -100;
+    console.log('raak');
+  }
+})
 
 function animate(){
   context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(animate);
   bird.pos.add(bird.vel);
-  if(bird.pos.dx < -100){
-    bird.pos.dx = canvas.width;
+  if(bird.pos.dx > canvas.width){
+    bird.pos.dx = -100;
     bird.pos.dy = Math.random()*canvas.height;
+
   }
-  context.drawImage(bird.image,bird.pos.dx,bird.pos.dy,100,100);
+  context.drawImage(bird.image,bird.pos.dx,bird.pos.dy,200,200);
 }
